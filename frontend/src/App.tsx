d76b175fbb0d4210a9b2adbac0ffbeb46db9a2a6
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-09 16:37:58 +0300

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='max-w-2xl mx-auto flex px-4'>
+        <div className='py-8 flex flex-col items-center'>
+          <h1 className='text-4xl font-bold uppercase mb-8'>
+            <span className='text-5xl'>URL to Video</span>
+            <br/>with power of AI
+          </h1>
+            <form className='grid gap-2'>
+              <input 
+              className='border-2 rounded-full bg-transparent text-white px-4 py-2 grow' 
+              type="url" 
+              placeholder="https://..."/>
+              <button 
+              className='bg-emerald-500 text-white font-semibold px-4 py-2 rounded-full'
+              type="submit">
+                Create&nbsp;video
+              </button>
+            </form>
+        </div>
+        <div className='p-8'>
+          <div className='bg-gray-200 w-[240px] h-[380px]'>
+            
+          </div>
+        </div>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
