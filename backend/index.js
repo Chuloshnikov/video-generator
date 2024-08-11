674fb7af0b9338018bcc98fe7d4976b7b704b80b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-11 14:18:06 +0300

    done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const uniqid = require('uniqid');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/test', (req, res) => {
-    return res.json('ok');
+    return res.json('ok3');
+});
+
+app.get('/create-story', (req, res) => {
+    const url = req.query.url;
+    const dir = uniqid();
+    const path = './stories/'+dir;
+    fs.mkdirSync(path, {recursive: true});
+    console.log({
+        url,
+    })
+    return res.json("ok");
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
